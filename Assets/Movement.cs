4eb60eb94db934b0398eba61d3d49fd6e91ada2b
Karol Bakalarz <k.bakalarz@itmcode.pl>
2025-03-18 13:26:14 +0100

    ppap

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,47 +1,60 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public GameObject firePoint;
     public Rigidbody2D Kebabi;
     public float moveSpeed = 10;
     private Vector2 userInput;
+
     public Sprite KebabiLeft;  
     public Sprite KebabiRight;
     public Sprite KebabiUp;
     public Sprite KebabiDown;
 
     private SpriteRenderer spriteRenderer;
+    private StrawberryShooter shooter;
+
+    private void Awake()
+    {
+        shooter = GetComponent<StrawberryShooter>();
+    }
+
     void Start()
     {
+        
         spriteRenderer = GetComponent<SpriteRenderer>();
         Kebabi = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        userInput.x = Input.GetAxisRaw("Horizontal");
+        userInput.x =Input.GetAxisRaw("Horizontal");
         userInput.y = Input.GetAxisRaw("Vertical");
-        if (userInput.x > 0 && userInput.y==0) 
+
+        shooter.UpdateShootingPosition((int)MathF.Ceiling(userInput.x), (int)MathF.Ceiling(userInput.y));
+    }
+    void FixedUpdate()
+    {
+        Kebabi.MovePosition(Kebabi.position + userInput * moveSpeed*Time.fixedDeltaTime);
+        if (userInput.x > 0 && userInput.y == 0)
         {
             spriteRenderer.sprite = KebabiRight;
         }
-        else if(userInput.x<0 && userInput.y==0)
+        if (userInput.x < 0 && userInput.y == 0)
         {
             spriteRenderer.sprite = KebabiLeft;
         }
-        else if(userInput.y>0)
+        if (userInput.y > 0)
         {
             spriteRenderer.sprite = KebabiUp;
         }
-        else if (userInput.y < 0)
+        if (userInput.y < 0)
         {
             spriteRenderer.sprite = KebabiDown;
         }
-    }
-
-    private void FixedUpdate()
-    {
-        Kebabi.MovePosition(Kebabi.position + userInput * moveSpeed);
     }
 }
--- Assets/StrawberryShooter.cs
+++ Assets/StrawberryShooter.cs
@@ -3,9 +3,40 @@ using UnityEngine;
 public class StrawberryShooter : MonoBehaviour
 {
     [SerializeField]GameObject Strawberry;
+    public GameObject upTransform;
+    public GameObject downTransform;
+    public GameObject leftTransform;
+    public GameObject rightTransform;
+
+    private Transform currentShootingTransform;
+    private Vector2 shootingDirection = Vector2.zero;
+
     void Start()
     {
         
+    }
+
+    public void UpdateShootingPosition(int x, int y)
+    {
+        shootingDirection = new (x, y);
+
+        if (x>0 && y==0)
+        {
+            currentShootingTransform = rightTransform.transform;
+        }
+        if (x <0 && y == 0)
+        {
+            currentShootingTransform = leftTransform.transform;
+        }
+        if ( y > 0)
+        {
+            currentShootingTransform = upTransform.transform;
+        }
+        if(y < 0)
+        {
+            currentShootingTransform = downTransform.transform;
+        }
+
     }
 
     // Update is called once per frame
@@ -13,7 +44,7 @@ public class StrawberryShooter : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-           Instantiate(Strawberry,transform.position,new Quaternion(0,0,0,0));
+           Instantiate(Strawberry, currentShootingTransform.position,new Quaternion(0,0,0,0));
         }
     }
 }
